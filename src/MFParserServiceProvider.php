6dd29ff584f71811384ff1f4f28d7ff07ff15e29
Klym <klimssalex@gmail.com>
2022-02-04 21:58:02 +0200

    new (MFParserServiceProvider): added ServiceProvider

---
+++ src/MFParserServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace EvolutionCMS\MFParser;
+
+use EvolutionCMS\ServiceProvider;
+
+class MFParserServiceProvider extends ServiceProvider
+{
+    protected string $namespace = 'MFParser';
+}
